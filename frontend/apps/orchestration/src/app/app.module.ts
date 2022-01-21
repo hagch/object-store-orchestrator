bf2eaf52fc98323b50b11b504ff8b0e36883acd4
Christoph Hagelkruys <christoph.hagelkruys@alanda.io>
2022-01-21 16:28:12 +0100

    changed frontend to nx workspace structure

---
+++ frontend/apps/orchestration/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'object-store',
+        loadChildren: () =>
+          loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://192.168.144.2:8889/remoteEntry.js',
+            exposedModule: './Module',
+          })
+            .then(m =>  m.RemoteEntryModule)
+      },
+    ], { initialNavigation: 'enabledBlocking' }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
